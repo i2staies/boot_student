11f3170cbe473967315dc1f0ea5f9c49c9fe1a0d
root <root@qq.com>
2022-07-16 08:38:14 +0800

    null

---
+++ src/main/java/com/itguigu/admin/config/MyDataSourceConfig.java
@@ -0,0 +1,57 @@
+package com.itguigu.admin.config;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.support.http.StatViewServlet;
+import com.alibaba.druid.support.http.WebStatFilter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+import java.util.Arrays;
+
+@Configuration
+public class MyDataSourceConfig {
+
+//  默认的自动配置是判断容器中没有才会配@ConditionalOnMissingBean(DataSource.class)
+    @Bean
+    @ConfigurationProperties("spring.datasource")
+    public DataSource dataSource() throws SQLException {
+        DruidDataSource druidDataSource = new DruidDataSource();
+        druidDataSource.setFilters("stat,wall");//想要开始数据源监控功能必须加入此属性
+        return druidDataSource;
+    }
+
+    /**
+     * 配置druid的监控页功能
+     * @return
+     */
+    @Bean
+    public ServletRegistrationBean statViewServlet(){
+        StatViewServlet statViewServlet = new StatViewServlet();
+//      通过什么才能访问监控页
+        ServletRegistrationBean<StatViewServlet> registrationBean = new ServletRegistrationBean<>(statViewServlet,"/druid/*");
+//      设置账号密码
+        registrationBean.addInitParameter("loginUsername", "admin");
+        registrationBean.addInitParameter("loginPassword", "admin");
+//      还可以设置哪些域名可以访问（在此不在演示）
+        return registrationBean;
+    }
+
+
+    /**
+     * webStatFilter： 用来采集web-jdbc关联监控的数据
+     * @return
+     */
+    @Bean
+    public FilterRegistrationBean webStatFilter(){
+        WebStatFilter webStatFilter = new WebStatFilter();
+        FilterRegistrationBean<WebStatFilter> webStatFilterFilterRegistrationBean = new FilterRegistrationBean<>(webStatFilter);
+        webStatFilterFilterRegistrationBean.setUrlPatterns(Arrays.asList("/*"));
+        webStatFilterFilterRegistrationBean.addInitParameter("exclusions", "*.js,*.gif,*.jpg,*.png,*.css,*.ico,/druid/*");
+        return webStatFilterFilterRegistrationBean;
+    }
+}
